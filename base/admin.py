04641ccb42a33abd1f21abc91239424db114eda9
Somdev Das <somdev4g@gmail.com>
2024-01-05 15:25:45 +0530

    Tag added to admin

--- base/admin.py
+++ base/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import BlogContent, UserProfile, UserComment, BlogPostLike
+from .models import BlogContent, UserProfile, UserComment, BlogPostLike, Tag
 from simple_history.admin import SimpleHistoryAdmin
 # Register your models here.
 admin.site.register(BlogContent, SimpleHistoryAdmin)
 admin.site.register(UserProfile, SimpleHistoryAdmin)
 admin.site.register(UserComment)
 admin.site.register(BlogPostLike)
+admin.site.register(Tag)
 
 
