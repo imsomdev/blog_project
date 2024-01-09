9e029a40e20cd2ef1daa4255cfcce4a098a33a88
Somdev Das <somdev4g@gmail.com>
2024-01-09 14:49:32 +0530

    swagger urls and settings added

--- base/urls.py
+++ base/urls.py
@@ -1,5 +1,21 @@
-from django.urls import path
+from django.urls import path, re_path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from .views import BlogPostLikeView, UserRegistrationView, UserLoginView, BlogContentView, BlogContentListView,UserProfileView, UserCommentView, DynamicSearch
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="My Blog",
+        default_version='v1',
+        description="Your API description",
+        terms_of_service="https://www.yourapp.com/terms/",
+        contact=openapi.Contact(email="contact@yourapp.com"),
+        license=openapi.License(name="Your License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('register', UserRegistrationView.as_view(), name='UserRegistrationView'),
@@ -12,5 +28,7 @@ urlpatterns = [
     path('profile/<str:username>', UserProfileView.as_view(), name='UserProfileViewPublic'),
     path('blog-posts/<int:pk>/comment', UserCommentView.as_view(), name = 'UserCommentView'),
     path('blog-posts/<int:pk>/like', BlogPostLikeView.as_view(), name='BlogPostLikeView'),
-    path('search', DynamicSearch.as_view(), name='DynamicSearch')
+    path('search', DynamicSearch.as_view(), name='DynamicSearch'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
--- base/views.py
+++ base/views.py
@@ -5,7 +5,16 @@ from rest_framework import status, generics, filters
 from .serializers import UserRegistrationSerializer, UserLoginSerializer, BlogContentSerializer, BlogContentListSerializer, UserProfileSerializer, UserCommentSerializer, BlogPostLikeSerializer
 from .models import BlogContent, UserProfile, UserComment, BlogPostLike 
 from django.contrib.auth.models import User
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+
 class UserRegistrationView(APIView):
+    @swagger_auto_schema(
+        request_body=UserRegistrationSerializer,
+        responses={201: UserRegistrationSerializer}
+    )
     def post(self, request):
         serializer = UserRegistrationSerializer(data=request.data)
         if serializer.is_valid():
@@ -15,14 +24,27 @@ class UserRegistrationView(APIView):
 
 
 class UserLoginView(APIView):
+    serializer_class = UserLoginSerializer
+
+    @swagger_auto_schema(
+        request_body=UserLoginSerializer,
+        responses={200: UserLoginSerializer}
+    )
     def post(self, request):
-        serializer = UserLoginSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        return Response({'username': user.username}, status=status.HTTP_200_OK)
+        user = User.objects.get(username=request.data['username'])
+        if not user.check_password(request.data['password']):
+            return Response('Invalid credentials', status=status.HTTP_401_UNAUTHORIZED)
+        refresh = RefreshToken.for_user(user)
+        return Response({'refresh': str(refresh), 'jwt': str(refresh.access_token)})
     
 
 class BlogContentView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(
+        request_body=BlogContentSerializer,
+        responses={201: BlogContentSerializer, 400: 'Bad Request', 401: 'Unauthorized'}
+    )
     def post(self, request):
         serializer = BlogContentSerializer(data=request.data)
         if serializer.is_valid():
@@ -34,6 +56,11 @@ class BlogContentView(APIView):
 
 
 class BlogContentListView(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    @swagger_auto_schema(
+        responses={200: BlogContentSerializer(many=True)}
+    )
     def get(self, request, pk=None):
         if pk is not None:
             try:
@@ -46,13 +73,17 @@ class BlogContentListView(APIView):
             blogs = BlogContent.objects.all()
             serializer = BlogContentListSerializer(blogs, many=True)
             return Response(serializer.data, status=status.HTTP_200_OK)
-    
 
+    @swagger_auto_schema(
+        request_body=BlogContentSerializer,
+        responses={200: BlogContentSerializer, 400: 'Bad Request', 403: 'Forbidden'}
+    )
     def put(self, request, pk):
         try:
             blog_post = BlogContent.objects.get(pk=pk)
         except BlogContent.DoesNotExist:
             raise Http404
+
         if blog_post.author == request.user or request.user.is_staff:
             serializer = BlogContentSerializer(blog_post, data=request.data)
             if serializer.is_valid():
@@ -61,14 +92,17 @@ class BlogContentListView(APIView):
             else:
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
-            return Response({'detail': 'You do not have permission to update this blog post.'},status=status.HTTP_403_FORBIDDEN)
-        
-    # To delete a blog post using pk
+            return Response({'detail': 'You do not have permission to update this blog post.'}, status=status.HTTP_403_FORBIDDEN)
+
+    @swagger_auto_schema(
+        responses={204: 'No Content', 403: 'Forbidden'}
+    )
     def delete(self, request, pk):
         try:
             blog_post = BlogContent.objects.get(pk=pk)
         except BlogContent.DoesNotExist:
             raise Http404
+
         if blog_post.author == request.user or request.user.is_staff:
             blog_post.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
@@ -77,34 +111,52 @@ class BlogContentListView(APIView):
 
 
 class UserProfileView(APIView):
-    def get(self, request, username):
-        try:
-            user = User.objects.get(username=username)
-            print(user)
-            user_profile = UserProfile.objects.get(user=user)
-            print(user_profile)
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    @swagger_auto_schema(
+        responses={200: UserProfileSerializer}
+    )
+    def get(self, request, username=None):
+        if not request.user.is_authenticated:
+            return Response({'detail': 'Please log in to view profiles'}, status=status.HTTP_401_UNAUTHORIZED)
+        if username is not None:
+            try:
+                user = User.objects.get(username=username)
+                user_profile = UserProfile.objects.get(user=user)
+                serializer = UserProfileSerializer(user_profile)
+                return Response(serializer.data)
+            except User.DoesNotExist:
+                raise Http404
+            except UserProfile.DoesNotExist:
+                raise Http404
+        else:
+            # Return the profile of the authenticated user
+            user_profile = UserProfile.objects.get(user=request.user)
             serializer = UserProfileSerializer(user_profile)
             return Response(serializer.data)
-        except User.DoesNotExist:
-            raise Http404
-        except UserProfile.DoesNotExist:
-            raise Http404
 
+    @swagger_auto_schema(
+        request_body=UserProfileSerializer,
+        responses={201: UserProfileSerializer}
+    )
     def post(self, request):
         existing_user = UserProfile.objects.filter(user=request.user).first()
         if existing_user:
-            return Response({'detail': 'User Profile Alreday Exists, Use Update to make'})
+            return Response({'detail': 'User Profile Already Exists, Use Update to make'})
         serializer = UserProfileSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
+    @swagger_auto_schema(
+        request_body=UserProfileSerializer,
+        responses={201: UserProfileSerializer}
+    )
     def put(self, request):
         existing_user = UserProfile.objects.filter(user=request.user).first()
         if not existing_user:
-            return Response ({'Profile not created, use POST to first create profile'}, status=status.HTTP_404_NOT_FOUND)
+            return Response({'detail': 'Profile not created, use POST to first create profile'}, status=status.HTTP_404_NOT_FOUND)
         serializer = UserProfileSerializer(existing_user, data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -114,9 +166,17 @@ class UserProfileView(APIView):
         
     
 class UserCommentView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(
+        request_body=UserCommentSerializer,
+        responses={201: UserCommentSerializer, 
+                   400: 'Bad Request', 
+                   401: 'Unauthorized', 
+                   404: 'Blog post not found'
+        }
+    )
     def post(self, request, pk):
-        if not request.user.is_authenticated:
-            return Response({'error': 'Please login first'}, status=status.HTTP_401_UNAUTHORIZED)
         try:
             blog_post = BlogContent.objects.get(pk=pk)
         except BlogContent.DoesNotExist:
@@ -131,15 +191,26 @@ class UserCommentView(APIView):
         
 
 class BlogPostLikeView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(
+        responses={
+            200: "Post disliked successfully",
+            201: "Post liked successfully",
+            400: "Bad Request",
+            401: "Unauthorized",
+            404: "Blog post not found",
+        }
+    )
     def post(self, request, pk):
-        if not request.user.is_authenticated:
-            return Response({'error': 'Please login first'}, status=status.HTTP_401_UNAUTHORIZED)
         try:
             blog_post = BlogContent.objects.get(pk=pk)
         except BlogContent.DoesNotExist:
             return Response({"error": "Blog post not found"}, status=status.HTTP_404_NOT_FOUND)
+
         # Check if the user has already liked the post
         existing_like = BlogPostLike.objects.filter(user=request.user, post=blog_post).first()
+
         if existing_like:
             # User has already liked the post, so dislike it
             existing_like.delete()
@@ -162,6 +233,10 @@ class DynamicSearch(generics.ListAPIView):
     search_fields = ['content', 'title']
     filter_backends = (filters.SearchFilter,)
 
+    @swagger_auto_schema(
+        responses={200: BlogContentSerializer(many=True),
+                   404: 'Not Found'}
+    )
     def get_queryset(self):
         search_param = self.request.query_params.get('search', None)
         if not search_param:
@@ -172,7 +247,6 @@ class DynamicSearch(generics.ListAPIView):
     def list(self, request):
         queryset = self.get_queryset()
         serializer = BlogContentSerializer(queryset, many=True)
-
         if not queryset.exists():
             return Response({'message': 'Please provide some valid search input'}, status=status.HTTP_404_NOT_FOUND)
         return Response(serializer.data)
