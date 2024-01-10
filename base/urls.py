43dd482221013ec93e5deb3bf7848288126360d9
Somdev Das <somdev4g@gmail.com>
2024-01-10 18:31:15 +0530

    removed profile/update url as per requirement

--- base/urls.py
+++ base/urls.py
@@ -24,7 +24,6 @@ urlpatterns = [
     path('blog-posts', BlogContentListView.as_view(), name='BlogContentListView'),
     path('blog-posts/<int:pk>', BlogContentListView.as_view(), name='BlogContentListView'), # For deleting 
     path('profile', UserProfileView.as_view(), name='UserProfileView'),
-    path('profile/update', UserProfileView.as_view(), name='UserProfileViewUpdate'),
     path('profile/<str:username>', UserProfileView.as_view(), name='UserProfileViewPublic'),
     path('blog-posts/<int:pk>/comment', UserCommentView.as_view(), name = 'UserCommentView'),
     path('blog-posts/<int:pk>/like', BlogPostLikeView.as_view(), name='BlogPostLikeView'),
