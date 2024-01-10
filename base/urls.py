feb856cde7feb04064ef2049a2c1cedfe9746997
Somdev Das <somdev4g@gmail.com>
2024-01-10 18:22:50 +0530

    added popular post, will test later

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, re_path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from .views import BlogPostLikeView, UserRegistrationView, UserLoginView, BlogContentView, BlogContentListView,UserProfileView, UserCommentView, DynamicSearchView, FilterRecentPostView
+from .views import BlogPostLikeView, UserRegistrationView, UserLoginView, BlogContentView, BlogContentListView,UserProfileView, UserCommentView, DynamicSearchView, FilterRecentPostView, PopularPostsView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -32,4 +32,5 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('recent-posts', FilterRecentPostView.as_view(), name='FilterRecentPostView'),
+    path('popular-post', PopularPostsView.as_view(), name='PopularPostsView'),
 ]
