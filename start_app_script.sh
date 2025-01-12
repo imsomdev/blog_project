49f76abb8925ffd5cc85a0ca5b5ecca67bd0754f
Somdev <somdev4g@gmail.com>
2025-01-12 23:56:54 +0530

    shell script updated

--- start_app_script.sh
+++ start_app_script.sh
@@ -1,3 +1,4 @@
-cd /home/ubuntu/blog_project;
-source venv/bin/activate;
-python manage.py runserver 0.0.0.0:8000
+#!/bin/bash
+cd /home/ubuntu/blog_project
+source venv/bin/activate
+uvicorn blog_project.asgi:application --host 0.0.0.0 --port 8000 --workers 4 --log-level debug
